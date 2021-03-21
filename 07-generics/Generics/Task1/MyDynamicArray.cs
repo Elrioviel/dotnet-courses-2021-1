2c39741a221b405c937292d8ffb5e3e2da3b8eca
Elrioviel <kristnaeve@gmail.com>
2021-03-22 00:53:33 +0300

    generics done

---
+++ 07-generics/Generics/Task1/MyDynamicArray.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    public class MyDynamicArray<T> 
+    {
+        T[] myArray;
+        int size;
+         
+
+        //1: array of 8 elements
+        public MyDynamicArray()
+        {
+            size = 8;
+            this.myArray = new T[size];
+            
+        }
+        //2: constructor of 1 parameter
+        public MyDynamicArray(int length)
+        {
+            myArray = new T[length];
+        }
+        //3:constructor w/ array parameter
+        public MyDynamicArray(IEnumerable<T> source)
+        {
+            myArray = source.ToArray();
+        }
+        //4: Add method and doubles the array's size and Adds element
+         public void Add(T obj)
+        {
+            
+            if (myArray[^1] != null)
+            {
+                var myArrayLength = myArray.Length;
+                Array.Resize(ref myArray, myArrayLength * 2);
+            }
+
+            var lastElement = Array.FindLast(myArray, x => x != null);
+            int index = Array.IndexOf(myArray, lastElement);
+            myArray[index] = obj;
+
+        }
+        //5: AddRange
+        public void AddRange (T[] elements)
+        {
+            var myArrayLength = myArray.Length;
+            Array.Resize<T>(ref myArray, myArrayLength + elements.Length);
+            Array.Copy(elements, 0, myArray, myArrayLength, elements.Length);
+        }
+        //6: delete a specific element from array
+        public T[] Remove(T toDeleteElement)
+        {
+            T[] finalArray = new T[myArray.Length-1];
+            int indexToDelete = Array.IndexOf(myArray, toDeleteElement);
+            Array.Copy(myArray, 0, finalArray, 0, indexToDelete);
+            Array.Copy(myArray, indexToDelete + 1, finalArray, indexToDelete, myArray.Length - indexToDelete - 1);
+            
+            return finalArray;
+
+        }
+
+        //7
+        public T[] Insert(T obj, int index)
+        {
+            try
+            {
+
+                T[] arrayObjInserted = new T[myArray.Length + 1];
+                T[] objToArray = new T[] { obj};
+
+                Array.Copy(myArray, 0, arrayObjInserted, 0, index);
+                Array.Copy(objToArray, 0, arrayObjInserted, index, 1);
+                Array.Copy(myArray, index, arrayObjInserted, index + 1, myArray.Length/2);
+
+                 return arrayObjInserted;
+            }
+            catch
+            {
+                throw new ArgumentOutOfRangeException("index");
+
+            }
+
+        }
+        //8
+        public int Length()
+        {
+            return myArray.Length;
+        }
+        //9
+        public int Capacity()
+        {
+            int count = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] != null) 
+                { count++; }
+
+            }
+
+            return count;
+        }
+        //10
+        public T this[int i]
+        {
+            get
+            {
+                return myArray[i];
+            }
+            set
+            {
+                if (i >= myArray.Length)
+                    throw new IndexOutOfRangeException($"The collection can hold only {myArray.Length} elements.");
+                else
+                myArray[i] = value;
+            }
+        }
+    }
+}
---
+++ 07-generics/Generics/Task1/Program.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //testing with object array
+
+            object[] array = new object[8];
+            array[0] = 1;
+            array[1] = "hello";
+            array[2] = 0.25;
+            array[3] = 'c';
+            array[4] = 4555745215658445578;
+            array[5] = "world";
+
+            MyDynamicArray<object> array2 = new MyDynamicArray<object>();
+            array2.Add("hi");
+
+
+            array2.AddRange(array);
+            int arrayLength = array2.Length();
+            int arrayCapacity = array2.Capacity();
+            Console.WriteLine("Array's length: {0}, array's capacity: {1}", arrayLength, arrayCapacity);
+            array2.Remove(0.25);
+            Console.WriteLine("Array's length: {0}, array's capacity: {1}", arrayLength, arrayCapacity);
+
+
+            //testing with integer array
+            int[] arrayOfInteger = new int[] { 1, 3, 6};
+            MyDynamicArray<int> arrayOfInteger2 = new MyDynamicArray<int>();
+            var arrayOfInteger3 = new MyDynamicArray<int>(new List<int> { 3, 5, 6, 7, 8, 9 });
+            
+
+            arrayOfInteger3.AddRange(arrayOfInteger);
+            Console.WriteLine("AddRange: ");
+            for (var i = 0; i < arrayOfInteger3.Length(); i++)
+            {
+                Console.WriteLine(arrayOfInteger3[i]);
+            }
+
+            int arrayOfIntegerLength = arrayOfInteger3.Length();
+            int arrayOfIntegerCapacity = arrayOfInteger3.Capacity();
+            Console.WriteLine("Array's length: {0}, capacity: {1}", arrayOfIntegerLength, arrayOfIntegerCapacity);
+            Console.WriteLine("\t************************");
+            
+            arrayOfInteger3.Add(35);
+            int arrayOfIntegerLengthAfterAdd = arrayOfInteger3.Length();
+            int arrayOfIntegerCapacityAfterAdd = arrayOfInteger3.Capacity();
+            Console.WriteLine("Array's length after adding one element: {0}, capacity: {1}", arrayOfIntegerLengthAfterAdd, arrayOfIntegerCapacityAfterAdd);
+            
+
+            Console.WriteLine("\t************************");
+            Console.WriteLine("Removing 8 from integer array:");
+
+            int [] arrayOfInteger4 = arrayOfInteger3.Remove(8);
+            for (var i = 0; i < arrayOfInteger4.Length; i++)
+            {
+                Console.WriteLine(arrayOfInteger4[i]);
+            }
+
+            Console.WriteLine("\t************************");
+            Console.WriteLine("Inserting '132' at index '5':");
+            int[] arrayOfInteger5 = arrayOfInteger3.Insert(132, 5);
+
+            for (var i = 0; i < arrayOfInteger5.Length; i++)
+            {
+                Console.WriteLine(arrayOfInteger5[i]);
+            }
+
+            Console.WriteLine("\t************************");
+            Console.WriteLine("Displaying the seventh element: {0}", arrayOfInteger5[7]);
+            
+        }
+    }
+
+}
