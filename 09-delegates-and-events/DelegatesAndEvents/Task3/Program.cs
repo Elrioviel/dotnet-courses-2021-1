27ee32485eddd90c14041b446a59c6d0271a6f0b
Elrioviel <kristnaeve@gmail.com>
2021-03-24 19:35:46 +0300

    All tasks done

---
+++ 09-delegates-and-events/DelegatesAndEvents/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static async void SortAsync(string [] myStringToSort, SortingModule.MyDelegate d1)
+        {
+            Console.WriteLine("sorting method started");
+            await Task.Run(() => SortingModule.Sort(myStringToSort, d1));
+            Console.WriteLine("\nsorting method finished");
+        }
+        static void Main(string[] args)
+        {
+            
+            string[] myStringToSort = new string[] { "hiiii", "hii", "hiii", "abc", "hi" };
+            SortingModule.MyDelegate d1 = new SortingModule.MyDelegate(SortingModule.CompareStrings);
+            SortAsync(myStringToSort,d1);
+            Console.Read();
+        }
+
+    }
+}
---
+++ 09-delegates-and-events/DelegatesAndEvents/Task3/SortingModule.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    
+    class SortingModule
+    {
+
+        public static int CompareStrings(string firstString, string secondString)
+        {
+            int compared;
+            if (firstString.Length > secondString.Length)
+            {
+                compared = 1;
+            }
+            else if (firstString.Length > secondString.Length)
+            {
+                compared = -1;
+            }
+            else
+            {
+                compared = string.Compare(firstString, secondString);
+
+            }
+            return compared;
+        }
+        public delegate int MyDelegate(string firstString, string secondString);
+        public static void Sort(string[] stringToSort, Delegate MyDelegate)
+        {
+            MyDelegate d1 = new MyDelegate(CompareStrings);
+            for (var i = 0; i < stringToSort.Length - 1; i++)
+            {
+                for (var j = i + 1; j < stringToSort.Length; j++)
+                {
+                    int biggerOrSmaller = d1(stringToSort[i], stringToSort[j]);
+                    if (biggerOrSmaller == 1)
+                    {
+                        string temp = stringToSort[i];
+                        stringToSort[i] = stringToSort[j];
+                        stringToSort[j] = temp;
+
+
+                    }
+                }
+            }
+            
+            for (var i = 0; i < stringToSort.Length; i++)
+            {
+                Console.Write("{0} | ", stringToSort[i]);
+            }
+            Thread.Sleep(500);
+            
+        }
+
+    }
+}
