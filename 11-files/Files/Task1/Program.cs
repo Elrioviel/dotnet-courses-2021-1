41a871890cdd9a70e15d2f4af3d5075028c730f8
Elrioviel <kristnaeve@gmail.com>
2021-04-01 17:51:29 +0300

    first task done

---
+++ 11-files/Files/Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    
+    class Program
+    {
+        public static void ProcessFile(string filePath)
+        {
+
+            if (File.Exists(filePath))
+            {
+                string fileContent = File.ReadAllText(filePath);
+                string[] integerStrings = fileContent.Split(new char[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                int[] integers = new int[integerStrings.Length];
+                for (int i = 0; i < integerStrings.Length; i++)
+                {
+                    integers[i] = int.Parse(integerStrings[i]);
+                }
+                for (int i = 0; i< integers.Length; i++)
+                {
+                    integers[i] *= integers[i];
+                    
+                }
+                File.WriteAllText(filePath, string.Empty);
+                File.WriteAllLines(filePath, Array.ConvertAll(integers, x => x.ToString()));
+                
+                
+            }
+            
+        }
+        public static void WriteFileContent(string filePath)
+        {
+            StreamReader sr = new StreamReader(filePath);
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+        }
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Ghalia\Desktop\Study\dotnet-courses-2021-1\11-files\Files\Task1\disposable_task_file.txt";
+            Console.WriteLine("File before changes:");
+            WriteFileContent(filePath);
+            Console.WriteLine("File processed:");
+            ProcessFile(filePath);
+            WriteFileContent(filePath);
+        }
+    }
+}
